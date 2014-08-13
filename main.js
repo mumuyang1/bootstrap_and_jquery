09044a57a47fc160401dd146c10492ab17c0ae7d
mumuyang1 <843624084@qq.com>
2014-08-13 18:47:39 +0800

    changing colour become perfect

--- main.js
+++ main.js
@@ -1,18 +1,6 @@
-//$(document).ready(function(){
-//
-//   $('.row').on('click',function(){
-//     if(mustInputs)
-//      $().()
-// } )
-//
-//});
-
-
-
 function getTotalPoints() {
 
-
-   var mustInputs = [
+    var mustInputs = [
         {
             id: 'class',
             text: '班级',
@@ -21,159 +9,156 @@ function getTotalPoints() {
         {
             id: 'number',
             text: '学号',
-            divId:'number_div'
+            divId: 'number_div'
         },
         {
             id: 'name',
             text: '姓名',
-            divId:'name_div'
+            divId: 'name_div'
         }
     ];
 
     if (hasEmptyMustInput(mustInputs)) {
+
         return false;
     }
-
-   return getScores();
+    return getScores();
 
 }
 
 function hasEmptyMustInput(inputs) {
 
+    var tag = true;
     for (var i = 0; i < inputs.length; i++) {
         var input = inputs[i];
         var element = document.getElementById(input.id);
 
         if (element && _.isEmpty(element.value)) {
-            alert('请填写' + input.text + '！');
-            $('#'+input.divId).addClass('has-error');
-
-            return true;
+            $('#' + input.divId).addClass('has-error');
+            tag =  false && tag;
+        }else {
+            $('#' + input.divId).removeClass('has-error');
         }
-
-            else{
-                   $('#'+input.divId).removeClass('has-error');
-
-                   }
-            }
-     return false;
     }
-function getBlankPoints(){
+    if(!tag){
+        alert('请填写完整个人信息！');
+        return true;
+    }
+    return false;
+}
+
+function getBlankPoints() {
 
     var blankItem1 = new Items(['统一建模语言'], 5);
-    var blankItem2 = new Items(['封装性','继承性','多态性'], 5);
+    var blankItem2 = new Items(['封装性', '继承性', '多态性'], 5);
     var answer1 = document.getElementById("blank1_1_1");
     var answer2 = [];
-    answer2.push( document.getElementById("blank1_2_1" ).value);
-    answer2.push( document.getElementById("blank1_2_2" ).value);
-    answer2.push( document.getElementById("blank1_2_3" ).value);
+    answer2.push(document.getElementById("blank1_2_1").value);
+    answer2.push(document.getElementById("blank1_2_2").value);
+    answer2.push(document.getElementById("blank1_2_3").value);
 
-     if (answer1.value === blankItem1.key ){
+    if (answer1.value === blankItem1.key) {
 
         blankItem1.points = blankItem1.score;
-     }
+    }
 
-     for(var i = 0; i < 3; i++){
+    for (var i = 0; i < 3; i++) {
 
-      for(var j = 0; j < 3; j++){
+        for (var j = 0; j < 3; j++) {
 
-        if(blankItem2.key[i] === answer2[j]){
+            if (blankItem2.key[i] === answer2[j]) {
 
-          blankItem2.points += blankItem2.score;
+                blankItem2.points += blankItem2.score;
 
-          break;
+                break;
 
             }
-          }
-       }
+        }
+    }
 
-       return   blankItem1.points + blankItem2.points;
+    return   blankItem1.points + blankItem2.points;
 
-  }
+}
 
 function getSelectionJudgePoints() {
 
-  var selectionJudge1 = new SelectionJudge('blank2_1');
-  var selectionJudge2 = new SelectionJudge('blank2_2');
-  var selectionJudge3 = new SelectionJudge('blank4_1');
-  var selectionJudge4 = new SelectionJudge('blank4_2');
+    var selectionJudge1 = new SelectionJudge('blank2_1');
+    var selectionJudge2 = new SelectionJudge('blank2_2');
+    var selectionJudge3 = new SelectionJudge('blank4_1');
+    var selectionJudge4 = new SelectionJudge('blank4_2');
 
-  var getValue1 = selectionJudge1.get();
-  var getValue2 = selectionJudge2.get();
-  var getValue3 = selectionJudge3.get();
-  var getValue4 = selectionJudge4.get();
+    var getValue1 = selectionJudge1.get();
+    var getValue2 = selectionJudge2.get();
+    var getValue3 = selectionJudge3.get();
+    var getValue4 = selectionJudge4.get();
 
-  var getValues = [getValue1,getValue2];
-  var getValuej = [getValue3,getValue4];
+    var getValues = [getValue1, getValue2];
+    var getValuej = [getValue3, getValue4];
 
-  var selectionJudges = new Items(['B','A'], 10);
-  var selectionJudgej = new Items(['x','v'], 10);
+    var selectionJudges = new Items(['B', 'A'], 10);
+    var selectionJudgej = new Items(['x', 'v'], 10);
 
-  for(var i = 0; i < getValues.length; i++){
+    for (var i = 0; i < getValues.length; i++) {
 
-    if(getValues[i] === selectionJudges.key[i]){
-      selectionJudges.points += selectionJudges.score;
+        if (getValues[i] === selectionJudges.key[i]) {
+            selectionJudges.points += selectionJudges.score;
 
-      }
+        }
     }
-  for(var k = 0; k < getValuej.length; k++){
+    for (var k = 0; k < getValuej.length; k++) {
 
-    if(getValuej[k] === selectionJudgej.key[k]){
-      selectionJudgej.points += selectionJudgej.score;
+        if (getValuej[k] === selectionJudgej.key[k]) {
+            selectionJudgej.points += selectionJudgej.score;
 
-      }
+        }
     }
-   return selectionJudges.points + selectionJudgej.points;
+    return selectionJudges.points + selectionJudgej.points;
 }
 
 function getMultiPoints() {
 
-  var value7 = document.getElementById("blank3_1_1").checked;
-  var value8 = document.getElementById("blank3_1_2").checked;
-  var value9 = document.getElementById("blank3_1_3").checked;
-  var value10 = document.getElementById("blank3_1_4").checked;
-  var value11 = document.getElementById("blank3_2_1").checked;
-  var value12 = document.getElementById("blank3_2_2").checked;
-  var value13 = document.getElementById("blank3_2_3").checked;
-  var value14 = document.getElementById("blank3_2_4").checked;
-  var MutiPoints1 = 0;
-  var MutiPoints2 = 0;
-  var multiAnswer1 = (value7 && value8 && value10) && (!value9);
-  var multiAnswer2 = (value11 && value12 && value13) && (!value14);
-  if(multiAnswer1){
-      MutiPoints1 = 10;
-     }
-  if(multiAnswer2) {
-      MutiPoints2 = 10;
-     }
-  var MutiPoints =MutiPoints1 + MutiPoints2;
-  return MutiPoints;
+    var value7 = document.getElementById("blank3_1_1").checked;
+    var value8 = document.getElementById("blank3_1_2").checked;
+    var value9 = document.getElementById("blank3_1_3").checked;
+    var value10 = document.getElementById("blank3_1_4").checked;
+    var value11 = document.getElementById("blank3_2_1").checked;
+    var value12 = document.getElementById("blank3_2_2").checked;
+    var value13 = document.getElementById("blank3_2_3").checked;
+    var value14 = document.getElementById("blank3_2_4").checked;
+    var MutiPoints1 = 0;
+    var MutiPoints2 = 0;
+    var multiAnswer1 = (value7 && value8 && value10) && (!value9);
+    var multiAnswer2 = (value11 && value12 && value13) && (!value14);
+    if (multiAnswer1) {
+        MutiPoints1 = 10;
+    }
+    if (multiAnswer2) {
+        MutiPoints2 = 10;
+    }
+    var MutiPoints = MutiPoints1 + MutiPoints2;
+    return MutiPoints;
 }
 
-function getShortAnswerPoints(){
+function getShortAnswerPoints() {
 
-  var value17 = document.getElementById("b");
-  var shortAnswer = new Items(['模型是对现实世界的简化和抽象,模型是对所研究的系统、过程、事物或概念的一种表达形式。可以是物理实体;可以是某种图形;或者是一种数学表达式。'], 20);
+    var value17 = document.getElementById("b");
+    var shortAnswer = new Items(['模型是对现实世界的简化和抽象,模型是对所研究的系统、过程、事物或概念的一种表达形式。可以是物理实体;可以是某种图形;或者是一种数学表达式。'], 20);
 
-  if(value17.value == shortAnswer.key[0]){
+    if (value17.value == shortAnswer.key[0]) {
 
-    shortAnswer.points = shortAnswer.score;
+        shortAnswer.points = shortAnswer.score;
     }
-
     return shortAnswer.points;
-  }
+}
 
+function getScores() {
 
+    var value18 = document.getElementById("scores");
+    var totalpoints = 0;
 
-function getScores(){
+    totalpoints = getBlankPoints() + getSelectionJudgePoints() + getMultiPoints() + getShortAnswerPoints();
+    value18.innerHTML = totalpoints;
+    $('#' + "scoresId").addClass('has-error');
 
-  var value18 = document.getElementById("scores");
-  var totalpoints = 0;
-
-  totalpoints = getBlankPoints() + getSelectionJudgePoints()+ getMultiPoints() + getShortAnswerPoints();
-  value18.innerHTML =totalpoints;
-  //value18.style.color = 'red';
-    $('#'+scoresId).addClass('has-error');
-
-  return false;
+    return false;
 }
